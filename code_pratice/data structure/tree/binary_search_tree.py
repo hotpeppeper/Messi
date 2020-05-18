ed4af78b594347fc35b2c777b09c1890400170c6
ludan <616403121@qq.com>
2020-05-18 16:23:42 +0800

    fix

--- code_pratice/data structure/tree/binary_search_tree.py
+++ code_pratice/data structure/tree/binary_search_tree.py
@@ -9,7 +9,7 @@ class Node(object):
     
 class BinarySearchTree(object):
     
-    def __init__(self, root):
+    def __init__(self, root=None):
         self.root = root
 
     def empty(self):
@@ -40,7 +40,7 @@ class BinarySearchTree(object):
 
     def insert(self, *values):
         for value in values:
-            self.insert(value)
+            self.__insert(value)
         return self
 
     def search(self, value):
@@ -54,24 +54,72 @@ class BinarySearchTree(object):
 
     def get_max(self, node=None):
         if node is None:
-            return self.root
+            node = self.root
         if not self.empty():
-            while node.rigth is not None:
+            while node.right is not None:
                 node = node.right
         return node
 
     def get_min(self, node=None):
         if node is None:
-            return self.root
+            node = self.root
 
         if not self.empty():
+            # node = self.root
             while node.left is not None:
                 node = node.left
 
         return node
 
 
+def binary_search_tree():
+    r"""
+    Example
+                  8
+                 / \
+                3   10
+               / \    \
+              1   6    14
+                 / \   /
+                4   7 13
+    >>> t = BinarySearchTree().insert(8, 3, 6, 1, 10, 14, 13, 4, 7)
+    >>> print(" ".join(repr(i.value) for i in t.traversal_tree()))
+    8 3 1 6 4 7 10 14 13
+    >>> print(" ".join(repr(i.value) for i in t.traversal_tree(postorder)))
+    1 4 7 6 3 13 14 10 8
+    >>> BinarySearchTree().search(6)
+    Traceback (most recent call last):
+    ...
+    IndexError: Warning: Tree is empty! please use another.
+    """
+    testlist = (8, 3, 6, 1, 10, 14, 13, 4, 7)
+    t = BinarySearchTree()
+    for i in testlist:
+        t.insert(i)
 
+    # Prints all the elements of the list in order traversal
+    print(t)
+
+    if t.search(6) is not None:
+        print("The value 6 exists")
+    else:
+        print("The value 6 doesn't exist")
+
+    if t.search(-1) is not None:
+        print("The value -1 exists")
+    else:
+        print("The value -1 doesn't exist")
+
+    if not t.empty():
+        print("Max Value: ", t.get_max().value)
+        print("Min Value: ", t.get_min().value)
+
+    # for i in testlist:
+    #     t.remove(i)
+    #     print(t)
+
+if __name__ == "__main__":
+    binary_search_tree()
     
                 
     
