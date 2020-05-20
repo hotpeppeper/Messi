3ae1dc3fb4e8e2de1b72a8cb64103fcb20dec67f
ludan <616403121@qq.com>
2020-05-20 18:19:04 +0800

    add

--- code_pratice/recursion/fibonacci.py
+++ code_pratice/recursion/fibonacci.py
@@ -7,5 +7,15 @@ def fibonacci(n):
         fib_table[n] = fibonacci(n - 1) + fibonacci(n - 2)
     return fib_table[n]
 
+def fibonacci_dp(n):
+    if n == 0 or n == 1:
+        return 1
+    fib = [0] * (n+1)
+    fib[1] = fib[2] = 1
+    for i in range(3, n+1):
+        fib[i] = fib[i-1] + fib[i-2]
+    return fib[n]
+
+print(fibonacci_dp(6))
 print(fibonacci(6))
 print(fib_table)
